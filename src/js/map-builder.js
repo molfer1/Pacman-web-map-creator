200fb5d28f2dd78c8c0fe0240f0cc1f32797e248
molfer.mjc <molfer.mjc@users.noreply.github.com>
2020-02-13 12:27:00 +0100

    finished view

--- src/js/map-builder.js
+++ src/js/map-builder.js
@@ -1,11 +1,15 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('map-builder.js loaded');
-    let line = 1;
     class Item {
         constructor(id) {
             this.id = '_' + id;
 
         }
+    }
+
+    function start() {
+        buildLeftSide();
+        console.log('UI loaded')
     }
     function buildLeftSide() {
         let x = 0, y = 0;
@@ -28,26 +32,52 @@ window.addEventListener('DOMContentLoaded', (event) => {
                 x = 0;
                 y++;
             }
-            if (y == 20) {
+            if (y == 40) {
+                buildRightSide()
                 break;
             }
         }
     }
-    buildLeftSide();
-
+    function buildRightSide() {
+        let x = 0, y = 0;
+        const map = document.getElementById('map');
+        while (true) {
+            const canvas = document.createElement('canvas');
+            canvas.classList.add('mapField');
+            canvas.id = 'x' + x + '_y' + y;
+            canvas.style.left = x * 27 + 'px';
+            canvas.style.top = y * 27 + 40 + 'px';
+            map.appendChild(canvas);
+            x++;
+            if (x == 27) {
+                const canvas = document.createElement('canvas');
+                canvas.classList.add('mapField');
+                canvas.id = 'x' + x + '_y' + y;
+                canvas.style.left = x * 27 + 'px';
+                canvas.style.top = y * 27 + 40 + 'px';
+                map.appendChild(canvas);
+                x = 0;
+                y++;
+            }
+            if (y == 30) {
+                break;
+            }
+        }
+    }
     function drawImg(canvas, x, y) {
-        let img = new Image();
+        const img = new Image();
         img.src = '../../data/img/sprites.png';
         let ctx = canvas.getContext('2d');
         img.addEventListener('load', function () {
-            if (line <= 20) {
+            if (y < 20) {
                 ctx.drawImage(img, 1 + x * 48, 1 + y * 48, 46, 46, 1, 1, canvas.width, canvas.height);
             }
-            else {
-                ctx.drawImage(img, 769 + x * 48, 1 + y * 48, 46, 46, 1, 1, canvas.width, canvas.height);
+            else if (y >= 20) {
+                ctx.drawImage(img, 769 + x * 48, 1 + (y - 20) * 48, 46, 46, 1, 1, canvas.width, canvas.height);
             }
         });
-        line++;
     };
+
+    start();
 });
 
