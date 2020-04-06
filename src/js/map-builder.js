657363ecf9b54aa44253857811f530ebfefe9b8c
molfer.mjc <molfer.mjc@users.noreply.github.com>
2020-04-07 01:44:45 +0200

    fieldSelector works, performance issues occured

--- src/js/map-builder.js
+++ src/js/map-builder.js
@@ -3,13 +3,13 @@ window.addEventListener('DOMContentLoaded', (e) => {
     const map = document.getElementById('map');
     const menu = document.getElementById('menu');
     const itemsTab = [];
-    var mapTab = [];
-    var selectorOrigin;
+    let mapTab = [];
+    let selectorOrigin;
     const img = new Image();
     img.src = './data/img/sprites1.png';
     img.addEventListener('load', function () {
         new Generator(this, 27);
-    })
+    });
 
     class Generator {
         constructor(img, size) {
@@ -43,6 +43,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
                     })
                 }
             }
+            console.log(mapTab);
         }
         drawItemsTile(x, y, img, size) {
             const canvas = document.createElement('canvas');
@@ -76,26 +77,29 @@ window.addEventListener('DOMContentLoaded', (e) => {
     }
     class Utilities {
         constructor() { }
-
-        toggleMenu(command) {
-            menu.style.display = command === "show" ? "block" : "none";
-            menuVisible = !menuVisible;
-        }
-
         setMenuPosition(origin) {
             menu.style.left = `${origin.left}px`;
             menu.style.top = `${origin.top}px`;
-            this.toggleMenu("show");
+            if(!menuVisible){this.toggleMenu();}
         }
-    };
+        toggleMenu(){
+            if(menuVisible) {
+                menuVisible = false;
+                menu.style.display = 'none';
+            }
+            else if(!menuVisible){
+                menuVisible = true;
+                menu.style.display = 'block';
+            }
+        }
+    }
     class Tiles {
         constructor() { }
-
         drawInSelectedTiles(x, y) {
             console.log('Item x: ' + x + ' y: ' + y);
             for (const raw of mapTab) {
                 for (const el of raw) {
-                    if (el.isSelected == true) {
+                    if (el.isSelected) {
                         const tile = document.getElementById(`c_${el.x}_${el.y}`);
                         tile.classList.remove('mapTile', 'selected', 'mapItem');
                         tile.classList.add('mapItem');
@@ -106,45 +110,57 @@ window.addEventListener('DOMContentLoaded', (e) => {
                 }
             }
         }
-
         selectSingleMapTile(x, y) {
             console.log('tile x: ' + x + ' y: ' + y);
             const mapTile = document.getElementById(`c_${x}_${y}`);
             if (mapTile.classList.contains('selected')) {
-                let index = mapTab[y].findIndex(function (el) { return el.x == x });
-                mapTab[y][index].isSelected = false;
+                mapTab[y][x].isSelected = false;
                 mapTile.classList.remove('selected');
             }
             else {
                 const selectedTiles = document.getElementsByClassName('selected');
+                console.log(selectedTiles);
                 let tilesCount = 0;
                 for (let i in selectedTiles) {
                     tilesCount++;
                 }
-                if (tilesCount == 3) {
+                if (tilesCount === 3) {
                     mapTile.classList.add('selected');
-                    let index = mapTab[y].findIndex(function (el) { return el.x == x });
+                    let index = mapTab[y].findIndex(function (el) { return el.x === x });
                     mapTab[y][index].isSelected = true;
                 }
                 else if (tilesCount > 3) {
                     if (crtlActive) {
                         mapTile.classList.add('selected');
-                        let index = mapTab[y].findIndex(function (el) { return el.x == x });
+                        let index = mapTab[y].findIndex(function (el) { return el.x === x });
                         mapTab[y][index].isSelected = true;
                     }
                     else {
-                        return;
                     }
                 }
 
             }
         }
         selectMultipleMapTiles() {
-            if (selectorActive) {
-                const selector = document.getElementById('selectorField');
-                selector.style.display = 'block';
-                selector.style.left = `${selectorOrigin.left}px`;
-                selector.style.top = `${selectorOrigin.top}px`;
+            console.log('multipleSelector');
+            if(!crtlActive){
+                if(this.checkIfAnyTilesAreSelected()){this.removeSelectedTiles()}
+            }
+            const selector = document.getElementById('selectorField');
+            let left = parseInt(selector.style.left.slice(0, -2)) - 600;
+            let top = parseInt(selector.style.top.slice(0, -2)) - 40;
+            let width = parseInt(selector.style.width.slice(0, -2));
+            let height = parseInt(selector.style.height.slice(0, -2));
+            let point1_x = Math.floor(left / 27);console.log('point1_x: '+ point1_x);
+            let point1_y = Math.floor(top / 27);console.log('point1_y: '+ point1_y);
+            let point2_x = Math.ceil((left + width)/27);console.log('point2_x: '+ point2_x);
+            let point2_y = Math.ceil((top + height)/27); console.log('point2_y: '+ point2_y);
+            for(let y = point1_y; y < point2_y; y++){
+                for(let x = point1_x; x < point2_x; x++){
+                    const mapTile = document.getElementById(`c_${x}_${y}`);
+                    mapTile.classList.add('selected');
+                    mapTab[y][x].isSelected = true;
+                }
             }
         }
         redrawImg(x, y, img, canvas) {
@@ -156,49 +172,67 @@ window.addEventListener('DOMContentLoaded', (e) => {
                 ctx.drawImage(img, 769 + x * 48, 1 + (y - 20) * 48, 46, 46, 1, 1, canvas.width, canvas.height);
             }
         }
-        getTileIndex() { }
+        checkIfAnyTilesAreSelected(){
+            const selectedTiles = document.getElementsByClassName('selected');
+            if(selectedTiles === []){return false} else{return true}
+        }
+        removeSelectedTiles() {
+            for (let y = 0; y < 30; y++) {
+                for (let x = 0; x <= 27; x++) {
+                    mapTab[y][x].isSelected = false;
+                }
+            }
+            const selectedTiles = document.getElementsByClassName('selected');
+            console.table(selectedTiles);
+            while(selectedTiles[0]) { // low performance (VERY LOW)
+                console.log('xd');
+                selectedTiles[0].classList.remove('selected');
+            }
+        }
     }
     let utilities = new Utilities;
     let tiles = new Tiles;
     let menuVisible = false;
     let crtlActive = false;
     let selectorActive = false;
+    console.table(mapTab);
 
     // EVENT LISTENERS *****
     window.addEventListener('click', e => {
-        if (menuVisible) {
-            utilities.toggleMenu('hide');
-        }
-    })
+        if (menuVisible) {utilities.toggleMenu()}
+    });
     window.addEventListener('contextmenu', e => {
+
         e.preventDefault();
         const origin = { left: e.pageX, top: e.pageY };
         utilities.setMenuPosition(origin);
-    })
+    });
     window.addEventListener('keydown', e => {
         console.log(e);
         if (e.keyCode === 27) {
-            utilities.toggleMenu('hide');
+            utilities.toggleMenu();
         }
         if (e.keyCode === 17) {
             crtlActive = true;
         }
-    })
+    });
     window.addEventListener('keyup', e => {
         if (e.keyCode === 17) {
             crtlActive = false;
         }
-    })
+    });
 
     // SELECTOR DIV
     map.addEventListener('mousedown', e => {
-        console.log('mousedown')
+        console.log('mousedown');
         selectorActive = true;
         selectorOrigin = { left: e.pageX, top: e.pageY };
-        tiles.selectMultipleMapTiles();
-    })
+        const selector = document.getElementById('selectorField');
+        selector.style.display = 'block';
+        selector.style.left = `${selectorOrigin.left}px`;
+        selector.style.top = `${selectorOrigin.top}px`;
+    });
     map.addEventListener('mousemove', e => {
-        console.log(e.pageX, e.pageY)
         if (selectorActive) {
             const selector = document.getElementById('selectorField');
             if ((e.pageX - selectorOrigin.left < 0) && (e.pageY - selectorOrigin.top >= 0)) { //LEFT
@@ -224,19 +258,20 @@ window.addEventListener('DOMContentLoaded', (e) => {
                 selector.style.width = e.pageX - selectorOrigin.left + 'px';
                 selector.style.height = e.pageY - selectorOrigin.top + 'px';
             }
+
         }
-        else { return }
-    })
+    });
     map.addEventListener('mouseup', e => {
-        console.log('mouseup')
+        console.log('mouseup');
         selectorActive = false;
+        tiles.selectMultipleMapTiles();
         const selector = document.getElementById('selectorField');
         selector.style.display = 'none';
         selector.style.left = `${0}px`;
         selector.style.top = `${0}px`;
         selector.style.width = `${0}px`;
         selector.style.height = `${0}px`;
-    })
-})
+    });
+});
 
 
